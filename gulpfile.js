7dde94bbf17b22330310917b95b2dd28cefe7654
oono0908 <damian_wkids@yahoo.co.jp>
2021-10-20 12:20:31 +0900

    imgmin

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ var plumber = require("gulp-plumber");
 var pug = require("gulp-pug");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const imagemin = require('gulp-imagemin');
+const mozjpeg = require('imagemin-mozjpeg');
+const pngquant = require('imagemin-pngquant');
+const changed = require('gulp-changed');
+const del = require('del');
 
 const paths = {
     src: {
@@ -48,8 +53,41 @@ gulp.task('sass', function(done){
         .pipe(gulp.dest('./dest//css/'));
         done();
 });
-gulp.task("watch", function() {
+
+gulp.task("imagemin", function (done) {
+  return gulp
+    .src('./src/img/**')
+    .pipe(changed('./dest/img'))
+    .pipe(
+      imagemin([
+        pngquant({
+          quality: [.60, .70], // 画質
+          speed: 1 // スピード
+        }),
+        mozjpeg({ quality: 65 }), // 画質
+        imagemin.svgo(),
+        imagemin.optipng(),
+        imagemin.gifsicle({ optimizationLevel: 3 }) // 圧縮率
+      ])
+    )
+    .pipe(gulp.dest('./dest/img'));
+    done();
+});
+
+gulp.task('delimg', function (done) {
+  return del('./dest/img');
+  done();
+});
+
+gulp.task('cleanimg', gulp.series(
+  'delimg',
+  'imagemin'
+));
+
+gulp.task("watch", function(done) {
     gulp.watch('./src/js/*.js', gulp.task('scripts'));
     gulp.watch('./src/sass/*.scss', gulp.task('sass'));
     gulp.watch(paths.src.pug, gulp.task("pug"));
+    gulp.watch('./src/img/**', gulp.task('imagemin'));
+    done();
 });
